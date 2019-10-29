6c5d21db682ca094a0a967a3a3afc48d934aa1af
Michael Eskarous <mneskarous@gmail.com>
2019-10-28 20:27:28 -0700

    Created RetiredForagerBee class

--- src/RetiredForager.js
+++ src/RetiredForager.js
@@ -1,3 +1,12 @@
 var RetiredForagerBee = function() {
+  ForagerBee.call(this);
+
+  this.age = 40;
+  this.job = 'gamble';
+  this.canFly = false;
+  this.color = 'grey';
 };
 
+RetiredForagerBee.prototype = Object.create(ForagerBee.prototype);
+
+RetiredForagerBee.prototype.constructor = RetiredForagerBee;
