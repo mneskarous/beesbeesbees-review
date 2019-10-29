3f8e03c80054946b889b7f66d19785ac825ffb7b
Michael Eskarous <mneskarous@gmail.com>
2019-10-28 20:23:25 -0700

    Created ForagerBee class

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,11 @@
 var ForagerBee = function() {
+  Bee.call(this);
+
+  this.job = 'find pollen';
+  this.canFly = true;
+  this.treasureChest = [];
 };
 
+ForagerBee.prototype = Object.create(Bee.prototype);
+
+ForagerBee.prototype.constructor = ForagerBee;
